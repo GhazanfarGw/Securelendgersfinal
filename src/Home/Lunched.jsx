1ada3a4af32f80fd4c40ad9d9ed128ca1a474f41
Ghazanfar Abbas <ghazanfargwi@gmail.com>
2026-04-15 03:23:42 +0500

    Update Lunched.jsx

--- src/Home/Lunched.jsx
+++ src/Home/Lunched.jsx
@@ -12,24 +12,44 @@ function About() {
   }, []);
 
   return (
-    <section className="relative bg-gradient-to-br from-[#f8fafc] to-[#eef2f7] py-20 px-6">
-      <div className="max-w-5xl mx-auto text-center mb-16" data-aos="fade-down">
-        <span className="bg-[#E8EDFF] text-[#2F6FD6] px-4 py-1 rounded-full text-sm">
+    <section className="relative bg-gradient-to-br from-[#f8fafc] to-[#eef2f7] md:py-20 py-12 px-6">
+      <div className="max-w-5xl mx-auto text-center md:mb-16 mb-5" data-aos="fade-down">
+        <span className="bg-[#E8EDFF] text-[#2F6FD6] px-4 py-1 rounded-full md:text-sm text-xs">
           Trusted Partner
         </span>
         {/* Premium Heading */}
           <h1 className="mt-2 md:mt-4 text-xl md:text-3xl font-bold text-[#1A2B6B] leading-tight">
-              Your Trusted Partner in Blockchain <br /> Security
+              Your Trusted Partner in Blockchain <br className="hidden xl:block lg:block"/> Security
           </h1>
-          <p className="text-gray-500 text-lg my-4">
+          <p className="text-gray-500 my-4 md:text-sm text-xs">
             The foundation of every relationship is trust.
           </p>
       </div>
       
       <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-16 items-center">
+        {/* RIGHT IMAGE */}
+        <div className="flex justify-center block xl:hidden lg:hidden" data-aos="zoom-in">
+          
+          <div className="relative">
+            
+            {/* Glow Effect */}
+            <div className="absolute inset-0 bg-blue-200 blur-3xl opacity-30 rounded-full"></div>
+
+            {/* Glass Card */}
+            <div className="relative bg-white/40 backdrop-blur-xl p-6 rounded-full shadow-xl">
+              <img
+                src="/Circle.png"
+                alt="Secure Ledger Logo"
+                className="w-52 object-contain"
+              />
+            </div>
+
+          </div>
+
+        </div>
         
         {/* LEFT CONTENT */}
-        <div data-aos="fade-up">
+        <div data-aos="fade-up" className="md:text-base text-sm">
           <p className="text-gray-600 leading-relaxed mb-5">
             At Secure Ledgers Solutions, we specialize in safeguarding your digital assets 
             and ensuring that all blockchain-related activities are secure and compliant.
@@ -48,7 +68,7 @@ function About() {
         </div>
 
         {/* RIGHT IMAGE */}
-        <div className="flex justify-center" data-aos="zoom-in">
+        <div className="flex justify-center hidden xl:block lg:block" data-aos="zoom-in">
           
           <div className="relative">
             
