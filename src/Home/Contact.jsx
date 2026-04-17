db27303e268b03426c9c16989a767a4668d1ae75
Ghazanfar Abbas <ghazanfargwi@gmail.com>
2026-04-18 02:00:45 +0500

    Update Contact.jsx

--- src/Home/Contact.jsx
+++ src/Home/Contact.jsx
@@ -94,7 +94,7 @@ export default function ContactServicePage() {
     <>
     <Header/>
     <div className="min-h-screen bg-gradient-to-br from-blue-900 via-blue-700 to-white flex items-center justify-center p-6">
-      <div className="w-full max-w-2xl bg-white/90 backdrop-blur-xl p-10 rounded-3xl shadow-2xl border border-blue-100">
+      <div className="w-full max-w-2xl bg-white/90 backdrop-blur-xl p-10 rounded-3xl shadow-2xl border border-blue-100 mt-20">
         <div className="text-center mb-8"> 
         </div>
 
